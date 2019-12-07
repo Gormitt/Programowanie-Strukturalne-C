797856abb3d233f94436a9c17761944ef0ce951d
Mateusz Wloczewski <mateusz.wloczewski00@gmail.com>
2019-12-07 15:41:06 +0100

    lab_09 udate

--- Laboratorium09/lab_9_wloczewski.c
+++ Laboratorium09/lab_9_wloczewski.c
@@ -1,6 +1,10 @@
 // Zadanie 62 - 66
 
 #include <stdio.h>
+#include <math.h>
+#include <limits.h>
+
+#define KONIEC_PROGRAMU printf("Koniec programu.\n\n");
 
 void CzyszczenieBufora() {
 	while (getchar() != '\n') {}
@@ -51,11 +55,63 @@ void Zadanie62() {
 			}
 		}
 	}
-	printf("Koniec programu.\n\n");
+	KONIEC_PROGRAMU
+}
+
+void Zadanie63() {
+	double pocz = 0.0;	// poczatek przedzialu
+	double kon = 0.125;	// koniec przedzialu
+	int ile = 33;		// ile punktow
+
+	double krok = (kon - pocz) / ((double)ile - 1);	//odl. miedzy pkt.
+	printf("Zadanie nr. 63 - program oblicza bledy przyblizenia\n");
+	printf("funkcji sin(x) przez x dla %d", ile);
+	printf(" rownoodleglych punktow z przedzialu");
+	printf(" [%.g,%g].\n\n%-14s", pocz, kon, "x");
+	printf("%-13s%-12s", "sin(x)", "x-sin(x)");
+	printf("%-12s\n", "|(x-sin(x))/x|");
+
+	for (int i = 0; i <= ile; i++) {
+		double x = pocz + i * krok;
+		double sin_x = sin(x);
+		double bl_b = x - sin_x;
+		double bl_w = x != 0.0 ? fabs(bl_b / x) : -1.0;
+		printf("\n%11.9f%14.9f%12.3e", x, sin_x, bl_b);
+		if (bl_w != -1.0)
+			printf("%12.3e", bl_w);
+	}
+	printf("\n");
+	KONIEC_PROGRAMU
+}
+
+void Zadanie64() {
+	long long liczba = INT_MAX - 5;
+	printf("%d\n", INT_MAX);
+	printf("Zadanie nr. 64 - program do wypisania 10 kolejnych liczb naturalnych, od max_int - 5\n");
+	for (int i = 1; i <= 10; i++) {
+		printf("liczba nr. %2d : %lld\n", i, liczba);
+		liczba++;
+	}
+	KONIEC_PROGRAMU
+}
+
+void Zadanie65() {
+	printf("Zadanie nr. 65 - program do wypisania n kolejnych potegi liczby 2\n");
+	int n, potega = 1;
+	do {
+		printf("podaj n: ");
+	} while (scanf_s("%d", &n) != 1);
+	for (int i = 0; i < n; i++) {
+		printf("wyliczenie nr. %d - 2 ^ %d = %d\n", i + 1, i, potega);
+		potega *= 2;
+	}
+	KONIEC_PROGRAMU
 }
 
 main() {
-	// Zadanie nr. 62
-	Zadanie62();
+	//Zadanie62();
+	//Zadanie63();
+	//Zadanie64();
+	Zadanie65();
 	return 0;
 }
