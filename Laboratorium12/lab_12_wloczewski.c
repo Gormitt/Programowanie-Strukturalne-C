911e6294d4bc35c24f04cde7d7b6b69f8ec26714
Mateusz Wloczewski <mateusz.wloczewski00@gmail.com>
2019-12-15 20:03:08 +0100

    lab_12 upload

---
+++ Laboratorium12/lab_12_wloczewski.c
@@ -0,0 +1,11 @@
+// Zadania 82 - 86
+
+#include <stdio.h>
+
+#define CZYSZCZENIE_BUFORA while(getchar() != '\n') {}
+#define ERROR_WCZYTYWANIE printf("ERROR - blad podczas wczytywania danych, prosze sprobuj ponownie: ");
+#define ERROR_ALOKACJA printf("ERROR - blad podczas alokowania miejsca na tablice\n");
+
+main() {
+	
+}
